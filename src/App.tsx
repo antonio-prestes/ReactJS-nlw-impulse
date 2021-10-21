93ad4101394239494d4327e775f3e61223f437ce
Diego <diegoantoniodasp@hotmail.com>
2021-10-20 22:56:22 -0300

    [NLW Heat] Mission: Impulse – Stage 2 Finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import styles from './app.module.scss'
 import {MessageList} from "./components/MessageList";
 import {LoginBox} from "./components/LoginBox";
+import {SendMessageForm} from "./components/SendMessageForm";
+import {useContext} from "react";
+import {AuthContext} from "./contexts/auth";
 
 
 export function App() {
+    const {user} = useContext(AuthContext)
+
     return (
-        <main className={styles.contentWrapper}>
+        <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
             <MessageList/>
-            <LoginBox/>
+            {!!user ? <SendMessageForm/> : <LoginBox/>}
         </main>
     )
 }
--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,45 +1,11 @@
 import styles from './styles.module.scss';
 import {VscGithubInverted} from "react-icons/vsc";
-import {useEffect} from "react";
-import {api} from "../../services/api";
+import {useContext, useEffect} from "react";
+import {AuthContext} from "../../contexts/auth";
 
-type AuthResponse = {
-    token: string;
-    user: {
-        id: string;
-        avatar_url: string;
-        name: string;
-        login: string;
-    }
-}
 
 export function LoginBox() {
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=2ec875773926987f728d`;
-
-    async function signIn(githubCode: string) {
-        const response = await api.post<AuthResponse>('auth', {
-            code: githubCode
-        })
-
-        const {token, user}= response.data;
-
-        localStorage.setItem('@dowhile:token', token)
-
-        console.log(user)
-    }
-
-    useEffect(() => {
-        const url = window.location.href;
-        const hasGithubCode = url.includes('?code=');
-
-        if (hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=')
-
-            window.history.pushState({}, '', urlWithoutCode)
-
-            signIn(githubCode);
-        }
-    })
+    const { signInUrl } = useContext(AuthContext);
 
     return (
         <div className={styles.loginBoxWrapper}>
